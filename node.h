9c80acfbd470dc3e3f14bf19dc3f4f50a4398c6b
Pablo Vinícios da S.Araújo <128994831+PabloVini28@users.noreply.github.com>
2023-07-04 23:53:36 -0300

    Update node.h

--- node.h
+++ node.h
@@ -1,33 +1,28 @@
 #ifndef NODE_H
 #define NODE_H
+
 #include <string>
+
+
 using namespace std;
 
-
 class Node {
-private: 
-    string code;
-    Node *left;  // ponteiro para filho esquerdo
-    Node *right;  // ponteiro para filho direito
+    private:
+    string question;
+    Node* left;
+    Node* right;
     
 public:
-    /**
-     * Construtor que recebe o valor que o nó irá armazenar e os ponteiros para os 
-     * filhos esquerdo e direito, respectivamente.
-     * */
-    Node(string pergunta,Node*l, Node *r);
-    
-    /**
-    Métodos gets e sets dos atributos
-    **/
+    Node(string question);
+
+    string getQuestion();
     Node* getLeft();
     Node* getRight();
-    string getCode();
-    
-    
-    void setLeft(Node* newLeft);
-    void setRight(Node* newRight);
-    void setCode(string newCode);
+
+    void setQuestion(string question);
+    void setLeft(Node* left);
+    void setRight(Node* right);
 };
 #include "node.cpp"
-#endif
+
+#endif  // NODE_H
