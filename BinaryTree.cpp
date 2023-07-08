4d55858e6a4d7c95a2767dd22523c6fde4a300e5
Pablo Vinícios da S.Araújo <128994831+PabloVini28@users.noreply.github.com>
2023-07-07 22:45:05 -0300

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,78 +1,139 @@
 #ifndef BINARYTREE_CPP
 #define BINARYTREE_CPP
-#include "binaryTree.h"
+
+#include "binarytree.h"
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
 using namespace std;
 
-BinaryTree::BinaryTree()
-{
-    setRoot(nullptr);
-}
+BinaryTree::BinaryTree(){
+  ifstream arquivo("SintomasMenor.txt");
 
-BinaryTree::~BinaryTree() {
-    destroyTree(getRoot());
-}
+  this->root = new Node(nullptr, nullptr);
 
-BinaryTree::BinaryTree(string pergunta) {
-    setRoot(new Node(pergunta));
-}
+  if (arquivo.is_open()){
+    int num_doencas;
+    arquivo >> num_doencas;
+    arquivo >> this->num_sintomas;
 
-bool BinaryTree::isEmpty() {
-    return getRoot() == nullptr;
-}
+    this->sintomas = new string[num_sintomas];
 
-Node* BinaryTree::getRoot() {
-    return root;
-}
+    // Usado para consumir o nome dos sintomas (informações nao utilizadas)
+    string buffer;
 
-void BinaryTree::setRoot(Node* root) {
-    this->root = root;
-}
+    arquivo.ignore();
 
-void BinaryTree::insertNode(string pergunta) {
-    Node* NovoNo = new Node(pergunta);
-    Node* atual = getRoot();
-    Node* parent = nullptr;
+    string doencas[num_doencas];
 
-    if (isEmpty()) {
-        setRoot(NovoNo);
-    } else {
-        while (atual != nullptr) {
-            parent = atual;
-            if (pergunta < atual->getQuestion()) {
-                atual = atual->getLeft();
-            } else {
-                atual = atual->getRight();
-            }
-        }
-        if (pergunta < parent->getQuestion()) {
-            parent->setLeft(NovoNo);
-        } else {
-            parent->setRight(NovoNo);
-        }
+    for (int i = 0; i < num_doencas; i++){
+      getline(arquivo, doencas[i]);
     }
-}
 
-void BinaryTree::printQuestions() {
-    Node* atual = getRoot();
-    if (isEmpty()) {
-        cout << "Arvore vazia." << endl;
-    } else {
-        while (atual != nullptr) {
-            cout << atual->getQuestion() << endl;
-            atual = atual->getLeft();
-        }
+    for (int i = 0; i < this->num_sintomas; i++){
+      getline(arquivo, this->sintomas[i]);
     }
+
+    while(getline(arquivo, buffer)){
+      int id_doenca;
+      int indice;
+
+      stringstream ss(buffer);
+
+      ss >> id_doenca;
+
+      Node* no_atual = this->root;
+      for(int i = 0; i < this->num_sintomas; i++){
+        ss >> indice;
+
+        if (indice == 0){
+          if (no_atual->getLeft() == nullptr){
+            no_atual->setLeft(new Node(nullptr, nullptr));
+            no_atual = no_atual->getLeft();
+          } else {
+            no_atual = no_atual->getLeft();
+          }
+        }
+
+        else if (indice == 1){
+          if (no_atual->getRight() == nullptr){
+            no_atual->setRight(new Node(nullptr, nullptr));
+            no_atual = no_atual->getRight();
+          } else {
+            no_atual = no_atual->getRight();
+          }
+        }
+      }
+
+      no_atual->adicionarDoenca(doencas[id_doenca-1]);
+    }
+    arquivo.close();
+  } else {
+    cout << "Erro ao abrir o arquivo 'SintomasMenor.txt'\n";
+  }
+
 }
 
-void BinaryTree::destroyTree(Node* node) {
-    if (node != nullptr) {
-        destroyTree(node->getLeft());
-        destroyTree(node->getRight());
+BinaryTree::~BinaryTree(){
+  clear();
+}
+
+string* BinaryTree::getSintomas(){
+  return this->sintomas;
+}
+
+int BinaryTree::getNumeroSintomas(){
+  return this->num_sintomas;
+}
+
+string BinaryTree::realizarConsulta(int sintomas[]){
+  Node* no_atual = this->root;
+
+  for(int i = 0; i < this->num_sintomas; i++){
+    if (no_atual == nullptr){
+      return "";
+    } else if (sintomas[i] == 0){
+      no_atual = no_atual->getLeft();
+    } else {
+      no_atual = no_atual->getRight();
+    }
+  }
+
+  string consulta = "";
+  for (string doenca : *no_atual->getDoencas()){
+    consulta += doenca + "\n";
+  }
+  
+  if (no_atual != nullptr){
+    return consulta;
+  } else {
+    return "";
+  }
+}
+
+void clear_rec(Node* node) {
+    if(node != nullptr) {
+        clear_rec(node->getLeft());
+        clear_rec(node->getRight());
         delete node;
     }
 }
 
+void BinaryTree::clear(){
+  clear_rec(root);
+  root = nullptr;
+}
+
+Node* BinaryTree::getRoot(){
+  return this->root;
+}
+
+void BinaryTree::setRoot(Node* root){
+  this->root = root;
+}
+
 #endif
-// Path: main.cpp
+
+[bina]
