fa458d395d69f41907bb4869c4295c894d03f1e5
Pablo Vinícios da S.Araújo <128994831+PabloVini28@users.noreply.github.com>
2023-07-07 22:46:28 -0300

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,38 +1,44 @@
 #ifndef NODE_CPP
 #define NODE_CPP
+
+#include <vector>
 #include "node.h"
 
-using namespace std;
+Node::Node(Node* left, Node* right){
+  this->doencas = nullptr;
+  this->left = left;
+  this->right = right;
+}
 
-Node::Node(string question){
-    setQuestion(question);
-    setLeft(nullptr);
-    setRight(nullptr);
-} 
-
-string Node::getQuestion(){
-    return question;
+Node::~Node(){
+  delete this->doencas;
 }
 
 Node* Node::getLeft(){
-    return left;
+  return this->left;
 }
 
-Node* Node::getRight() {
-    return right;
+Node* Node::getRight(){
+  return this->right;
 }
 
-void Node::setQuestion(string question) {
-    this->question = question;
+void Node::setLeft(Node* left){
+  this->left = left;
 }
 
-void Node::setLeft(Node* left) {
-    this->left = left;
+void Node::setRight(Node* right){
+  this->right = right;
 }
 
-void Node::setRight(Node* right) {
-    this->right = right;
+vector<string>* Node::getDoencas(){
+  return this->doencas;
 }
 
+void Node::adicionarDoenca(string doenca){
+  if (this->doencas == nullptr){
+    this->doencas = new vector<string>();
+  }
+  this->doencas->push_back(doenca);
+}
 
-#endif  // NODE_CPP
+#endif
